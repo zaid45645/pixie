6edcecdaf428c1a1fb40533a47b55a32333dac36
Mohammed Zaid <70617604+zaid45645@users.noreply.github.com>
2025-09-02 21:45:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from components import * 
+from components import *
 import webbrowser
 
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Pixie!~")
 
-def grid(rows, cols, color):
+def create_grid(rows, cols, color):
     grid = []
 
     for i in range(rows):
         grid.append([])
-        for _ in range(colums):
+        for _ in range(cols):
             grid[i].append(color)
     
     return grid
@@ -24,7 +24,6 @@ def draw_grid(win, grid):
             pygame.draw.line(window, black_colour, (0, i * pixel_sizes), (width, i * pixel_sizes))
         for j in range(colums + 1):
             pygame.draw.line(window, black_colour, (j * pixel_sizes, 0), (j * pixel_sizes, height - toolbar_h))
-
 
 def draw(window, grid, buttons):
     window.fill(bg_colour)
@@ -46,7 +45,8 @@ def get_row_col_from_pos(pos):
 
 run = True
 clock_fps = pygame.time.Clock()
-grid = grid(rows, colums, bg_colour)
+grid = create_grid(rows, colums, bg_colour)
+drawing_color = black_colour
 undo_stack = []
 redo_stack = []
 max_undo_steps = 10
@@ -64,7 +64,6 @@ buttons = [
     Button(550, button_y, 50, 50, white_colour, ":D", black_colour)
 ]
 
-
 while run:
     clock_fps.tick(fps)
     for event in pygame.event.get():
@@ -81,13 +80,11 @@ while run:
                         continue
                     drawing_color = button.color
                     if button.text == "Reset":
-                        grid = grid(rows, colums, bg_colour)
+                        grid = create_grid(rows, colums, bg_colour)
                         drawing_color = black_colour
                     if button.text == ":D":
                         webbrowser.open("https://www.youtube.com/watch?v=xvFZjo5PgG0")
                         
-
-    
     draw(window, grid, buttons)
 
 pygame.quit()
